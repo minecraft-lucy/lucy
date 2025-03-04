fbc2fc52c310f9214f143b580836ec68ab962148
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2025-03-04 23:57:50 +0800

    refactor: Remove dead code and create package for source mojang

---
+++ remote/mojang/mojang.go
@@ -0,0 +1,32 @@
+package mojang
+
+import (
+	"encoding/json"
+	"io"
+	"lucy/datatypes"
+	"net/http"
+)
+
+const VersionManifestURL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
+
+func getVersionManifest() (manifest *datatypes.VersionManifest, err error) {
+	manifest = &datatypes.VersionManifest{}
+
+	// TODO: Add cache mechanism if http call is too slow or fails
+	resp, err := http.Get(VersionManifestURL)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, manifest)
+	if err != nil {
+		return nil, err
+	}
+
+	return manifest, nil
+}
