c92cbcf28965e8bd9ecc88c0ef5e0acff3f036b7
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2025-04-18 08:01:23 +0800

    chore: Legal information

--- copyright.sh
+++ copyright.sh
@@ -1,4 +1,5 @@
-/*
+#!/bin/bash
+COPYRIGHT='/*
 Copyright 2024 4rcadia
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,5 +14,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+'
 
-package structout
+cd $(dirname "$0") || exit 1
+find . -name "*.go" -type f -not -path "*/\.*" | xargs grep -L "Copyright 2024 4rcadia" | while read file; do
+    echo "$COPYRIGHT" >temp_file
+    cat "$file" >>temp_file
+    mv temp_file "$file"
+done
