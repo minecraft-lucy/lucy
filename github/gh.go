16c4221c190c425a5e41dbc67f662943c7148ca9
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-02-13 13:30:35 -0500

    fix(remote): Fix GitHub API request and version inference for MCDR

--- github/gh.go
+++ github/gh.go
@@ -15,22 +15,36 @@ func GetFileFromGitHub(apiEndpoint string) (
 	msg *GhApiMessage,
 	data []byte,
 ) {
-	res, err := http.Get(apiEndpoint)
+	resp, err := http.Get(apiEndpoint)
 	if err != nil {
 		return err, nil, nil
 	}
-	defer tools.CloseReader(res.Body, logger.Warn)
-	data, err = io.ReadAll(res.Body)
+	defer tools.CloseReader(resp.Body, logger.Warn)
+	data, err = io.ReadAll(resp.Body)
 	if err != nil {
 		return err, nil, nil
 	}
 
+	// Check if the response is an error message from GitHub API
 	err = json.Unmarshal(data, &msg)
+	if err == nil && msg != nil && msg.Message != "" {
+		return nil, msg, data
+	}
+
+	var item GhItem
+	err = json.Unmarshal(data, &item)
 	if err != nil {
 		return fmt.Errorf("%w: %w", ErrCannotDecode, err), nil, nil
 	}
-	if msg != nil && msg.Message != "" {
-		return nil, msg, data
+	resp, err = http.Get(item.DownloadUrl)
+	if err != nil {
+		return err, nil, nil
 	}
+	defer tools.CloseReader(resp.Body, logger.Warn)
+	data, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return err, nil, nil
+	}
+
 	return nil, nil, data
 }
